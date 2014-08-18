2748f8eb506def151e2ac19a6eb81ec212119e41
Henrik <info@henriklindstrom.se>
2014-08-18 10:54:30 +0200

    You can now unlock camera with "L"

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,39 +7,63 @@ public class CameraController : MonoBehaviour {
     private Vector3 currentLookAt = Vector3.zero;
     public float lerpSpeed = 1f;
     public Vector3 offset;
-    public float xMax;
-    public float xMin;
-    public float zMax;
-    public float zMin;
     public float offsetYMin = 10f;
     public float offsetYMax = 100f;
     public float zoomSpeed = 10f;
+    public bool locked = true;
+    public float scrollSpeed = 10f;
+
+    public Transform topLeftCorner;
+    public Transform bottomRightCorner;
 
     public Vector3 standbyPos;
 
     void Update () {
-        if (target != null)
+        if (Input.GetButtonUp("Camera lock"))
         {
-            offset.y -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-            offset.y = Mathf.Clamp(offset.y, offsetYMin, offsetYMax);
+            locked = !locked;
+        }
 
-            var desiredLookAt = new Vector3(
-                Mathf.Clamp(target.position.x, xMin, xMax),
-                target.position.y,
-                Mathf.Clamp(target.position.z, zMin, zMax));
+        offset.y -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        offset.y = Mathf.Clamp(offset.y, offsetYMin, offsetYMax);
+
+        if (target != null && locked)
+        {
+            var desiredLookAt = target.position;
 
             if (currentLookAt != desiredLookAt)
             {
                 currentLookAt = Vector3.Lerp(currentLookAt, desiredLookAt, Time.deltaTime * lerpSpeed);
             }
 
-            transform.position = currentLookAt + offset;
-            transform.LookAt(currentLookAt);
         }
         else
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.forward);
-            transform.position = standbyPos;
+            if (Input.mousePosition.x < 10)
+            {
+                currentLookAt += Vector3.left * scrollSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.x > (Screen.width - 10))
+            {
+                currentLookAt += Vector3.right * scrollSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y < 10)
+            {
+                currentLookAt += Vector3.back * scrollSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y > (Screen.height - 10))
+            {
+                currentLookAt += Vector3.forward * scrollSpeed * Time.deltaTime;
+            }
         }
+
+        currentLookAt = new Vector3(
+            Mathf.Clamp(currentLookAt.x, topLeftCorner.position.x, bottomRightCorner.position.x),
+            currentLookAt.y,
+            Mathf.Clamp(currentLookAt.z, bottomRightCorner.position.z, topLeftCorner.position.z));
+
+        transform.position = currentLookAt + offset;
+        transform.LookAt(currentLookAt);
+
     }
 }
