a523294e676a928ca1545c3181a7bdc9d5a4ab08
Patrik Skog <Patrik92s@hotmail.com>
2014-09-16 15:24:41 +0200

    Added a scrollingtexture script, that does what it sounds like. Made the waterfall more smooth, with alot lower resulotion texture thx to the script. Also it will do alot when we start adding special effects with abilites. (looked alot on how the effect is made in WOW). Great example as they try to do it as efficently as possible.

---
+++ Assets/TerrainV1Assets/WaterfallFolder/ScrollingTexture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour {
+
+	public float scrollSpeed = 0.5f;
+	public float offset;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		offset += (Time.deltaTime*scrollSpeed)/10.0f;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}
