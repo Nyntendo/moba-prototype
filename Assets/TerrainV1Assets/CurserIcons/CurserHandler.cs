ca33ad703b5dd24f4c12895799f489daf31ab3fc
Patrik Skog <Patrik92s@hotmail.com>
2014-09-20 14:56:35 +0200

    Cursor update -Script taking care of what icon and what to do on certain cursor events. -Addind move marker when rightclicking, ray sent from main camera, wont work when clicking on the minimap :c -The destoryscript that is applyed to the move marker object is not optimal, because the old markers should be removed when adding a new one, so a new kind of system needs to be implemented.

---
+++ Assets/TerrainV1Assets/CurserIcons/CurserHandler.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurserHandler : MonoBehaviour {
+
+
+	public Texture2D normalCursor;
+	public Texture2D clickedCursor;
+	private Texture2D currentCursor;
+	public int cursorSize = 32;
+
+	public Camera rayCamera;
+	public GameObject MoveMarker;
+	private Ray ray;
+	private RaycastHit hit;
+	
+	void Start () {
+		Screen.showCursor = false;
+		currentCursor = normalCursor;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			OnLeftMouseUp();
+		}
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			OnLeftMouseDown();
+		}
+		if (Input.GetMouseButtonDown (1)) 
+		{
+			OnRightMouseDown();
+		}
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorSize, cursorSize), currentCursor);
+	}
+
+	void OnLeftMouseUp()
+	{
+		currentCursor = normalCursor;
+		cursorSize = 32;
+	}
+
+	void OnLeftMouseDown()
+	{
+		cursorSize = 24;
+	}
+
+	void OnRightMouseDown()
+	{
+		print ("U CLICKED");
+		ray = rayCamera.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit)) 
+		{
+			GameObject obj = Instantiate (MoveMarker, new Vector3(hit.point.x, hit.point.y + 1, hit.point.z), Quaternion.identity) as GameObject;
+		}
+	}
+
+}
