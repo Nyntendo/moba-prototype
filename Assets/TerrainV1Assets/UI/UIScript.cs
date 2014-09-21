88f8e4373d7f11e50a653daef32e88bc849776ac
Patrik Skog <Patrik92s@hotmail.com>
2014-09-21 22:54:58 +0200

    added UI background, might need some rework. Added a bool so the ui can be turned off and on in runtime. (see UIHandler.)

---
+++ Assets/TerrainV1Assets/UI/UIScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+
+	public Texture2D Frame;
+	public bool ShowUI;
+	private int ScreenHeight;
+	private int ScreenWidth;
+
+	private int lastScreenWidth;
+	private int lastScreenHeight;
+
+	// Use this for initialization
+	void Start () {
+		ScreenHeight = Screen.height;
+		ScreenWidth = Screen.width;
+		lastScreenHeight = ScreenHeight;
+		lastScreenWidth = ScreenWidth;
+	}
+	void Update () 
+	{
+		if (lastScreenWidth != Screen.width || lastScreenHeight != Screen.height) 
+		
+		{
+			ScreenHeight = Screen.height;
+			ScreenWidth = Screen.width;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (ShowUI == true) 
+		{
+			GUI.depth = 10;
+			GUI.DrawTexture (new Rect (0, 0, ScreenWidth, ScreenHeight), Frame);
+		}
+	}
+}
